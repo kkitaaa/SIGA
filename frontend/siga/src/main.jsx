909646e0289390c8c18fcd6cb888d4e678db9ca7
Maria Isabel <msoto2024@alu.uct.cl>
2026-05-07 14:15:54 -0400

    Login y Registro

--- frontend/siga/src/main.jsx
+++ frontend/siga/src/main.jsx
@@ -1,5 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-<ChakraProvider>
-  <App />
-</ChakraProvider>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
