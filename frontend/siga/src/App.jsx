974a6697a214bd166ddcdc0809919afba658f75d
Maria Isabel <msoto2024@alu.uct.cl>
2026-05-09 12:13:52 -0400

    Actualicé las rutas

--- frontend/siga/src/App.jsx
+++ frontend/siga/src/App.jsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
+import Login from "./pages/login";
+import Roles from "./pages/roles";
+// PONER A FUTURO
+//import Register from "./pages/Register";
+//import Dashboard from "./pages/Dashboard";
+//import Profile from "./pages/Profile";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/roles" element={<Roles />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+// PONER A FUTURO DENTRO DE <Routes>:
+// <Route path="/register" element={<Register />} />
+// <Route path="/dashboard" element={<Dashboard />} />
+// <Route path="/profile" element={<Profile />} />
---
+++ frontend/siga/src/routes.jsx
@@ -0,0 +1,9 @@
+import Login from './pages/login'; 
+import Roles from './pages/roles';
+
+const routes = [
+    { path: '/', component: Login },
+    { path: '/roles', component: Roles },
+];
+
+export default routes;
