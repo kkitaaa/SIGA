52c992642a6b19f452b55d000d12d3facc01daea
kkitaaa <102261574+kkitaaa@users.noreply.github.com>
2026-05-13 19:14:27 -0400

    Cambios mínimos
    Se cambió el package.json ya que faltaba algo y además se agregó un mensaje para comprobar si funciona el auth.

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,4 +6,8 @@ const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 
+router.get("/", (req, res) => {
+  res.send("Auth funcionando");
+});
+
 export default router;
