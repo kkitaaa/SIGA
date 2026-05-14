518691752d6ed97917c02548209e0de30907716e
kkitaaa <102261574+kkitaaa@users.noreply.github.com>
2026-05-14 19:07:46 -0400

    Instalación y configuración SwaggerUI

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import { swaggerUi, swaggerSpec } from "./docs/swagger.js";
 
 dotenv.config();
 
@@ -10,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   res.send("SIGA backend corriendo correctamente");
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,54 @@ import { register, login } from "../controllers/auth.controller.js";
 
 const router = Router();
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Registrar usuario
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Usuario creado
+ */
 router.post("/register", register);
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Iniciar sesión
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Login exitoso
+ *       401:
+ *         description: Credenciales incorrectas
+ */
 router.post("/login", login);
 
 router.get("/", (req, res) => {
