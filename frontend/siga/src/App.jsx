5d9e32e69c5ee87d11890864ab02b4696857f704
kkitaaa <102261574+kkitaaa@users.noreply.github.com>
2026-06-28 13:39:16 -0400

    Arreglos

--- frontend/siga/src/App.jsx
+++ frontend/siga/src/App.jsx
@@ -1,67 +1,88 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
+import ProtectedRoute from "./middleware/ProtectedRoute";
+
+// Auth
 import Login from "./pages/login";
+
+// General
 import Home from "./pages/home";
+
+// Roles
 import AsignacionRoles from "./pages/asignacion_roles";
+
+// PIE
 import PieDashboard from "./pages/home_pie";
 import ProfesoresDashboard from "./pages/home_profesores";
 import AsignacionPIEPage from "./pages/asignacion_PIE";
 
-import ProtectedRoute from "./middleware/ProtectedRoute";
-
-// PONER A FUTURO
-// import Register from "./pages/Register";
-// import Dashboard from "./pages/Dashboard";
-// import Profile from "./pages/Profile";
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Pública */}
-        <Route path="/" element={<Login />} />
+    <Routes>
 
-        {/* Cualquier usuario autenticado */}
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
+      {/* 🔓 Públicas */}
+      <Route path="/" element={<Login />} />
 
-        {/* Solo Administrativos */}
-        <Route
-          path="/asignacion-roles"
-          element={
-            <ProtectedRoute roles={["Administrativo"]}>
-              <AsignacionRoles />
-            </ProtectedRoute>
-          }
-        />
+      {/* 🏠 Home general */}
+      <Route
+        path="/home"
+        element={
+          <ProtectedRoute rolesPermitidos={[
+            "Directiva",
+            "Administracion",
+            "Equipo PIE",
+            "Profesor",
+            "Funcionario",
+            "Coordinador PIE",
+            "Coordinador Administrativo"
+          ]}>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
 
-        {/* Equipo PIE */}
-        <Route
-          path="/pie"
-          element={
-            <ProtectedRoute roles={["Equipo PIE"]}>
-              <PieDashboard />
-            </ProtectedRoute>
-          }
-        />
+      {/* 👤 Roles */}
+      <Route
+        path="/asignacion-roles"
+        element={
+          <ProtectedRoute rolesPermitidos={["Directiva"]}>
+            <AsignacionRoles />
+          </ProtectedRoute>
+        }
+      />
 
-        {/* Profesores */}
-        <Route
-          path="/profesores"
-          element={
-            <ProtectedRoute roles={["Profesor"]}>
-              <ProfesoresDashboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+      {/* 🧩 PIE */}
+      <Route
+        path="/pie"
+        element={
+          <ProtectedRoute rolesPermitidos={["Equipo PIE"]}>
+            <PieDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/asignacion-pie"
+        element={
+          <ProtectedRoute rolesPermitidos={["Equipo PIE", "Coordinador PIE"]}>
+            <AsignacionPIEPage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profesores"
+        element={
+          <ProtectedRoute rolesPermitidos={["Profesor"]}>
+            <ProfesoresDashboard />
+          </ProtectedRoute>
+        }
+      />
+
+      {/* 🚫 fallback */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+
+    </Routes>
   );
 }
 
