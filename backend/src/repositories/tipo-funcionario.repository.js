8b5b01a861bef33a4c59b117c2f7d6606dab2d43
cofranco <chacotaco06@gmail.com>
2026-06-29 02:28:50 -0400

    tarea: gestion de funcionario
    falta testeo y correccion de posibles errores

---
+++ backend/src/repositories/tipo-funcionario.repository.js
@@ -0,0 +1,33 @@
+import { prisma } from "../config/prisma.js";
+
+export class TipoFuncionarioRepository {
+  async crear(data) {
+    return prisma.tipoFuncionario.create({ data });
+  }
+
+  async listar() {
+    return prisma.tipoFuncionario.findMany();
+  }
+
+  async obtenerPorId(id) {
+    return prisma.tipoFuncionario.findUnique({ where: { id_tipo_funcionario: id } });
+  }
+
+  async actualizar(id, data) {
+    return prisma.tipoFuncionario.update({
+      where: { id_tipo_funcionario: id },
+      data,
+    });
+  }
+
+  async desactivar(id) {
+    return prisma.tipoFuncionario.update({
+      where: { id_tipo_funcionario: id },
+      data: { activo: false },
+    });
+  }
+
+  async buscarPorNombre(nombre) {
+    return prisma.tipoFuncionario.findUnique({ where: { nombre } });
+  }
+}
